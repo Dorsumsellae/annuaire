da2d8501ea0685fbc151cc0285abbb663c027578
Dorsumsellae <nicolas.moutonbesson@gmail.com>
2022-02-19 11:44:29 +0100

    add validator to registerForm

--- src/app/personne-ajouter/personne-ajouter.component.ts
+++ src/app/personne-ajouter/personne-ajouter.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, NgForm, Validators } from '@angular/forms';
 import { Personne } from '../models/personne';
 import { PersonneServiceService } from '../services/personne-service.service';
 
@@ -11,13 +11,39 @@ import { PersonneServiceService } from '../services/personne-service.service';
 export class PersonneAjouterComponent implements OnInit {
   constructor(private ps: PersonneServiceService) {}
 
+  firstname = new FormControl('', Validators.required);
+  lastname = new FormControl('', Validators.required);
+  phoneNumber = new FormControl('', [Validators.pattern('[- +()0-9]{12}')]);
+
   ngOnInit(): void {}
 
-  public traiterFormulaire(form: NgForm) {
-    console.log(form.value);
+  public traiterFormulaire() {
+    //console.log(form.value);
     //ajouter la personne à la liste
-    this.ps.ajouterPersonne(this.formValueToPersonne(form.value));
+    if (
+      !this.firstname.invalid &&
+      !this.lastname.invalid &&
+      !this.phoneNumber.invalid
+    ) {
+      this.ps.ajouterPersonne(this.reactiveFormTopersonne());
+      this.resetForm();
+    }
   }
+
+  reactiveFormTopersonne(): Personne {
+    return {
+      firstname: this.firstname.value,
+      lastname: this.lastname.value,
+      phoneNumber: this.phoneNumber.value,
+    } as Personne;
+  }
+
+  resetForm(): void {
+    this.firstname.reset();
+    this.lastname.reset();
+    this.phoneNumber.reset();
+  }
+
   formValueToPersonne(formValue: any): Personne {
     return {
       firstname: formValue.firstname.toLowerCase(),
